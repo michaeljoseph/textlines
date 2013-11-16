800ce7600a8660f6eb05be39f632ac63f34c8ef1
Michael Joseph <michaeljoseph+github@gmail.com>
2013-11-16 15:33:22 +0200

    Reading time

--- tests/test_textlines.py
+++ tests/test_textlines.py
@@ -9,7 +9,8 @@ class TestTextLines(BaseTestCase):
         expected_text = (
             '27 :abc: 2 :page_with_curl: '
             '0.27 % of a :notebook: 0.04 % of a :books: '
-            '0m12s :speech_balloon:'
+            '0m16s :speech_balloon: '
+            '0m5s :eyes::book:'
         )
         self.assertEquals(
             expected_text,
