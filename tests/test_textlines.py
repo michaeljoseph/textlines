36bfaf353f7a6db0a7170455b195f7dd05c9c146
Michael Joseph <michaeljoseph+github@gmail.com>
2013-11-21 13:34:37 +0200

    Use humanize

--- tests/test_textlines.py
+++ tests/test_textlines.py
@@ -8,9 +8,9 @@ class TestTextLines(BaseTestCase):
     def test_textlines_docs(self):
         expected_text = (
             '27 :abc: 2 :page_with_curl: '
-            '0.27 % of a :notebook: 0.04 % of a :books: '
-            '0m16s :speech_balloon: '
-            '0m5s :eyes::book:'
+            '0.27% of a :notebook: 0.04% of a :books: '
+            'a moment :speech_balloon: '
+            'a moment :eyes::book:'
         )
         self.assertEquals(
             expected_text,
--- textlines/__init__.py
+++ textlines/__init__.py
@@ -2,18 +2,22 @@
 Sparklines for text. text_lines counts your words, paragraphs, pages
 and emits a short summary.
 
-The text doesn't need to be here, but I'm trying to write a new paragraph.
+This text doesn't need to be here, but I'm trying to write a new paragraph.
 """
 __author__ = 'Michael Joseph'
 __email__ = 'michaeljoseph@gmail.com'
 __url__ = 'https://github.com/michaeljoseph/textlines'
 __version__ = '0.0.1'
 
+import humanize
+from datetime import timedelta
+
+
 SHORT_STORY = 10000
 NOVEL = 60000
 PAGE = 450
-WPM = 100
-RPM = 300
+TPM = 100  # talking works per minute
+RPM = 300  # reading words per minute
 
 EMOJI = {
     'word': ':abc:',
@@ -26,25 +30,21 @@ EMOJI = {
 }
 
 
-def human_duration(minutes):
-    return '%dm%ds' % (minutes, minutes * 60)
-
-
 def reading_time(num_words):
-    return human_duration(
-        (num_words * 1.0) / RPM
+    return humanize.time.naturaldelta(
+        timedelta(minutes=num_words / RPM),
     )
 
 
 def talking_time(num_words):
-    return human_duration(
-        (num_words * 1.0) / WPM
+    return humanize.time.naturaldelta(
+        timedelta(minutes=num_words / TPM),
     )
 
 
 def work_percentage(words, length):
     percentage = (len(words) * 1.0 / length) * 100
-    return '%.2f %%' % percentage
+    return '%.2f%%' % percentage
 
 
 def text_lines(words):
